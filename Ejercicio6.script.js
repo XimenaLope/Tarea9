b3b6e6a70b8da7bf21df36b45665d6dd53214f5d
Ximena Lope Guevara <99635154+XimenaLope@users.noreply.github.com>
2023-05-01 10:35:20 -0600

    script.js

---
+++ Ejercicio6.script.js
@@ -0,0 +1,16 @@
+function abrirRegalo(event) {
+    const image = event.target;
+    image.removeEventListener('click', abrirRegalo);
+
+    const regaloAbierto = document.querySelector('#regalo-abierto');
+    const regaloCerrado = document.querySelector('#regalo-cerrado');
+
+
+    regaloCerrado.classList.add('oculto');
+    regaloAbierto.classList.remove('oculto');
+
+
+}
+
+const image = document.querySelector('#regalo-cerrado img');
+image.addEventListener('click', abrirRegalo);
